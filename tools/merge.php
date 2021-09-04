f76aebf6d9833257a04c6e01c026b824cecb00bf
Esad Man <4535141+mandza@users.noreply.github.com>
2021-09-04 15:10:21 +0300

    Update merge.php
    Changed multiple dashed line with str_pad command for auto aligning.

--- tools/merge.php
+++ tools/merge.php
@@ -53,7 +53,7 @@
     $outputCode = '';
     
     foreach($fileList as $fileName) {
-        $outputCode .= "\n\n".'//---- '.basename($fileName).' -----------------------------'."\n\n";
+        $outputCode .= "\n\n".'// '.str_pad(basename($fileName),50,"-",STR_PAD_BOTH)."\n\n";
         $anotherCode = file_get_contents($fileName);
         $anotherCode = preg_replace ('/^<\?php/', '', $anotherCode);
         $anotherCode = preg_replace ('/\?>\*$/', '', $anotherCode);
